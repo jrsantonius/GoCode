bf1991be6d24aecd522127f368d5689cd6a980ba
ASUSPC\Asus <jrsantoniusyonanda@gmail.com>
2016-09-17 12:43:59 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	_ "sabtu/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/auth"
+	_ "github.com/go-sql-driver/mysql"
+	
+)
+
+func init() {
+	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/sabtu")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.InsertFilter("*",beego.BeforeRouter,auth.Basic("username","password"))
+	beego.Run()
+}
+
